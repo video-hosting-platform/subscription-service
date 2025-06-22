885a85f7fdfda8ff39cd0cf7e8988ed0b0027f5e
redblood-pixel <danka2202@yandex.ru>
2025-06-22 23:29:35 +0300

    chore: init
    Добавил первую пустую миграцию
    Примерно накидал  каркас проекта
    Добавил docker compose для postgres

---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/video-hosting-platform/subscription-service/internal"
+
+func main() {
+	internal.Run()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/video-hosting-platform/subscription-service
+
+go 1.24.1
