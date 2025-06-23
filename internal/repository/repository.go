d2ce237f97202eb3c3b7a34cb6c90b6b244aa5aa
redblood-pixel <danka2202@yandex.ru>
2025-06-23 13:29:57 +0300

    feat: Добавлены интерфейсы service и repository

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,4 +1,14 @@
 package repository
 
 type Repository struct {
+	SubscribeRepository
+}
+
+type SubscribeRepository interface {
+	CreateSubscription(authorID, userID int) error
+	IncrementSubscriptions(authorID int) error
+	DecrementSubscriptions(authoID int) error
+	GetSubscriptionStatus(authorID, userID int) (bool, error)
+	GetSubscriptions(userID int) ([]int, error)
+	GetSubscriptionCount(authorID int) (int, error)
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,8 +1,14 @@
 package service
 
 type Service struct {
+	SubscriptionsService
 }
 
 type SubscriptionsService interface {
-	
+	Subscribe(authorID, userID int) error
+	Unsubscribe(authorID, userID int) error
+
+	GetSubscriptions(userID int) ([]int, error)
+	GetSubscriptionStatus(authorID int) (bool, error)
+	GetSubscriptionCount(authorID int) (int, error)
 }
